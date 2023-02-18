2f581052f44665b6b4add91bb17e21fd7632e99f
mmansilla11 <mmansilla@bdgsa.net>
2023-02-17 18:58:04 -0600

    Finalización de ejercicio

---
+++ Silo/Program.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+try
+{
+    using IHost host = await StartSiloAsync();
+    Console.WriteLine("\n\n El Host de Silo está en proceso, presionar Enter para terminar...\n\n");
+    Console.ReadLine();
+
+    await host.StopAsync();
+
+    return 0;
+}
+catch (Exception ex)
+{
+    Console.WriteLine(ex);
+    return 1;
+}
+static async Task<IHost> StartSiloAsync()
+{
+    var builder = new HostBuilder()
+        .UseOrleans(silo =>
+        {
+            silo.UseLocalhostClustering()
+                .ConfigureLogging(logging => logging.AddConsole());
+        });
+
+    var host = builder.Build();
+    await host.StartAsync();
+
+    return host;
+}
